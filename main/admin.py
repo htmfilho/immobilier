65fe60925660640146456d105f3047892dc3ae49
Hildeberto Mendonca <me@hildeberto.com>
2015-06-06 11:46:49 +0200

    CRUD for personne, assurance et banque.

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Personne
+from .models import Assurance
+from .models import Banque
 
-admin.site.register(Post)
+admin.site.register(Personne)
+admin.site.register(Assurance)
+admin.site.register(Banque)
