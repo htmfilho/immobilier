e12a78d882883644587684732932e166e14aaac5
Hildeberto Mendonca <me@hildeberto.com>
2015-06-11 11:14:22 +0200

    Added entities Batiment, Location and Locataire

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Personne
 from .models import Assurance
 from .models import Banque
+from .models import Batiment
+from .models import Location
+from .models import Locataire
 
 admin.site.register(Personne)
 admin.site.register(Assurance)
 admin.site.register(Banque)
+admin.site.register(Batiment)
+admin.site.register(Location)
+admin.site.register(Locataire)
